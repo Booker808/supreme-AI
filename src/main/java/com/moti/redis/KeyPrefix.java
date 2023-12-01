44f42739582db96ca6734aacebfc50fa8314aff0
booker <m18770027426@163.com>
2023-12-02 01:26:00 +0800

    搭建

--- src/main/java/com/moti/redis/KeyPrefix.java
+++
@@ -1,8 +0,0 @@
-package com.moti.redis;
-
-public interface KeyPrefix {
-		
-
-	public String getPrefix();
-	
-}
---
+++ supremeai-business/src/main/java/com/supremeai/supremeaibusiness/SupremeaiBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.supremeai.supremeaibusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupremeaiBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupremeaiBusinessApplication.class, args);
+    }
+
+}
